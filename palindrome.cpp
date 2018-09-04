19096c8c326176247b57c3f0dd3ad6732b8925dd
U-BSD\385975 <385975@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-04 11:42:17 -0700

    started palindrome, getting input

---
+++ palindrome.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+  char input[80];
+
+  cin.get(input, 80);
+  cin.get();
+
+  cout << input << endl;
+  return 0;
+}
