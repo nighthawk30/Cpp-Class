df3b2d1373bd0abc0a885f8a666722870261d66a
nighthawk30 <nt.rip.diver@gmail.com>
2018-09-05 00:45:01 +0100

    Added character comparison and output statement

--- palindrome.cpp
+++ palindrome.cpp
@@ -8,7 +8,8 @@ int main()
   char input[80];
   char* censored = new char[80];
   int arrayindex = 0;
-
+  bool palindrome = true;
+  
   cin.get(input, sizeof(input));
   cin.get();
 
@@ -16,14 +17,30 @@ int main()
     {
       if (input[i] > 57)//the cut off point for ascii numbers
 	{
-	  censored[arrayindex] = input[i];
+	  censored[arrayindex] = tolower(input[i]);
 	  arrayindex++;
 	}
     }
 
   censored[arrayindex] = '\0';
-  censored
   
-  cout << censored << endl;
+  //Compare string sides character front to back
+  for (int i = 0; censored[i] != '\0'; i++)
+    {
+      if (censored[i] != censored[sizeof(censored) - i - 2])
+	{
+	  palindrome = false;
+	}
+    }
+  
+  if (palindrome)
+    {
+      cout << "Palindrome" << endl;
+    }
+  else
+    {
+      cout << "Not a Palindrome" << endl;
+    }
+      
   return 0;
 }
