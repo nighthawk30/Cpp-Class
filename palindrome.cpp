26519274a31e6a088fdeb4c57d66bdd2d45e3f6f
U-BSD\385975 <385975@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-04 12:25:23 -0700

    string censoring

--- palindrome.cpp
+++ palindrome.cpp
@@ -6,10 +6,24 @@ using namespace std;
 int main()
 {
   char input[80];
+  char* censored = new char[80];
+  int arrayindex = 0;
 
-  cin.get(input, 80);
+  cin.get(input, sizeof(input));
   cin.get();
 
-  cout << input << endl;
+  for (int i = 0; input[i] != '\0'; i++)
+    {
+      if (input[i] > 57)//the cut off point for ascii numbers
+	{
+	  censored[arrayindex] = input[i];
+	  arrayindex++;
+	}
+    }
+
+  censored[arrayindex] = '\0';
+  censored
+  
+  cout << censored << endl;
   return 0;
 }
