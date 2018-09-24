c02c827597dce3f2db5b6e18d28f5b81cead9b5f
U-BSD\385975 <385975@SHS-10L74C2E7.beaverton.k12.or.us>
2018-09-24 12:26:20 -0700

    Added GPA part of studentadd and started work on putting in and getting it out of a vector

--- StudentList/stdlst.cpp
+++ StudentList/stdlst.cpp
@@ -6,12 +6,9 @@ Student info database
 #include <iostream>
 #include <cstring>
 #include <vector>
+#include <cmath>
 
 using namespace std;
-
-void studentAdd();
-void listPrint();
-void studentDelete();
 
 //information held on the students
 struct Student
@@ -21,6 +18,10 @@ struct Student
   int studentID;
   float GPA;
 };
+
+void studentAdd(vector <Student*>* list);
+void listPrint();
+void studentDelete();
 
 int main()
 {
@@ -32,14 +33,14 @@ int main()
   cout << "'add' - Add a new student" << endl;
   cout << "'print' - Print out the list of students and their information" << endl;
   cout << "'delete' - Delete a student by their id number" << endl;
-  cout << "'quit' - exit the program" << endl << endl;
+  cout << "'quit' - exit the program" << endl;
 
   while (command != 'q')
     {
       if (command == 'a')
 	{
 	  cout << endl << "Add Function" << endl;
-	  studentAdd();
+	  studentAdd(list);
 	}
       else if (command == 'p')
 	{
@@ -51,17 +52,16 @@ int main()
 	}
       
       command = '-';
-      cout << "Command: ";
+      cout << endl << "Command: ";
       cin.get(command);
       while (cin.get() != '\n');
     }
-
     
   return 0;
 }
 
 //creates a new student and adds it to the vector
-void studentAdd()
+void studentAdd(vector <Student*>* list)
 {
   char input[10];
   Student* initial = new Student;//use Student intial; to create a local student struct and Student* initial = new Student; to create a Global/pointer struct
@@ -76,19 +76,49 @@ void studentAdd()
   cin.get(input, 10);
   while (cin.get() != '\n');
   strcpy(initial -> nameLast, input);
-  cout << "Name: " << initial -> nameLast << endl;
+  //cout << "Name: " << initial -> nameLast << endl;
 
   //ID #
-  cout << "First Name: ";
+  cout << "ID Number: ";
   cin.get(input, 10);
   while (cin.get() != '\n');
   int idnum = 0;
   for (int i = 0; i < strlen(input); i++)
     {
-
+      idnum += (input[i] - 48) * pow(10,strlen(input) - i - 1);
     }
-  strcpy(initial -> nameFirst, input);
+  initial -> studentID = idnum;
+  //cout << "ID: " << initial -> studentID << endl;
 
+  //GPA
+  cout << "GPA: ";
+  cin.get(input, 10);
+  while (cin.get() != '\n');
+  float gpa = 0;
+  int power = 0;
+  int decimal = 0;
+  for (int i = 0; i < strlen(input); i++)
+    {
+      if (input[i] == '.')
+	{
+	  decimal = i;
+	  break;
+	}
+    }
+  power = decimal - 1;
+  for (int i = 0; i < strlen(input); i++)
+    {
+      if (i != decimal)
+	{
+	  gpa += (input[i] - 48) * pow(10, power--);
+	}
+    }
+  cout << gpa << endl;
+  initial -> GPA = gpa;
+  
+  //Add student to the vector
+  list -> push_back(initial);
+  cout << list[0] -> GPA << endl; //vector begin
   return;
   //create student with given info + add it to vector of students
 }
